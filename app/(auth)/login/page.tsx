3407a4d53f12deeed501e0b499744354a0d22546
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2025-02-05 20:41:40 -0300

    feat: add page login and componentes de UI, integrar Supabase

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+import LoginForm from "@/components/login-form";
+import React from "react";
+
+function LoginPage() {
+  return (
+    <div className="flex h-screen w-full px-4 justify-center items-center">
+        <LoginForm />
+    </div>
+  );
+}
+
+export default LoginPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="bg-background w-screen h-screen">Home</div>
   );
 }
